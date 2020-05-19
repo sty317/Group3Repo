a31a9d42aa92013bf3033e1e00c2509ed4307cdb
sty317 <65366770+sty317@users.noreply.github.com>
2020-05-18 20:26:35 -0700

    Create mcosttest.py

---
+++ manufactures/mcosttest.py
@@ -0,0 +1,22 @@
+
+import unittest
+
+from manufactures.mcost_comp import MCcomp
+from openmdao.api import Problem
+from openmdao.utils.assert_utils import assert_check_partials
+
+
+class TestCLComp(unittest.TestCase):
+
+    def test_component_and_derivatives(self):
+        prob = Problem()
+        prob.model = MCcomp()
+        prob.setup()
+        prob.run_model()
+
+        data = prob.check_partials(out_stream=None)
+        assert_check_partials(data, atol=1.e-3, rtol=1.e-3)
+
+
+if __name__ == '__main__':
+    unittest.main()
